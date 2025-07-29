a11a0099a6b9549da8ec300da44268cdcb5d5108
SUNKARA NAVYA SRI SAI <navyasunkara049@gmail.com>
2025-07-29 22:34:02 +0530

    Time: 396 ms (18%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,2 +1,8 @@
-# Write your MySQL query statement below
-SELECT NAME FROM EMPLOYEE WHERE ID IN(SELECT MANAGERID FROM EMPLOYEE GROUP BY MANAGERID HAVING COUNT(*)>=5)
+SELECT E1.name
+FROM Employee E1
+JOIN (
+    SELECT managerId, COUNT(*) AS directReports
+    FROM Employee
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5
+) E2 ON E1.id = E2.managerId;
