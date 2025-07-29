79d2321fad56ba0d1b04c5b2b3d94a8e95837164
SUNKARA NAVYA SRI SAI <navyasunkara049@gmail.com>
2025-07-29 21:28:03 +0530

    Time: 1272 ms (34.88%), Space: 0B (100%) - LeetHub

--- 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,2 @@
-SELECT euni.unique_id, e.name 
-FROM Employees e  
-LEFT JOIN EmployeeUNI euni  
-ON e.id = euni.id;
+# Write your MySQL query statement below
+select euni.unique_id,e.name from Employees  e  left join  EmployeeUNI   euni on e.id=euni.id;
