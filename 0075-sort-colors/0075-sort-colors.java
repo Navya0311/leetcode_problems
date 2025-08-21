45032ca171862437e096d71531dd50b30f472449
SUNKARA NAVYA SRI SAI <navyasunkara049@gmail.com>
2025-08-21 21:14:53 +0530

    Time: 1 ms (20.93%), Space: 42.2 MB (17.98%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        
+        for(int i=0;i<nums.length;i++)
+        {
+         Arrays.sort(nums);
+        }
+      
+    
+    }
+}
