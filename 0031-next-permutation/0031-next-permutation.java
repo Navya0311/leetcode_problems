03b42a452b9d4fef2f7f99a234b575505955d55a
SUNKARA NAVYA SRI SAI <navyasunkara049@gmail.com>
2025-09-14 12:25:49 +0530

    Time: 0 ms (100%), Space: 43.5 MB (11.5%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int index = -1;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index == -1) {
+            reverse(nums, 0, n - 1);
+            return;
+        }
+
+        for (int j = n - 1; j > index; j--) {
+            if (nums[j] > nums[index]) {
+                swap(nums, index, j);
+                break;
+            }
+        }
+
+        reverse(nums, index + 1, n - 1);
+    }
+
+    void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    void reverse(int[] nums, int start, int end) {
+        for (int i = start, j = end; i < j; i++, j--) {
+            swap(nums, i, j);
+        }
+    }
+}
