cfd158b7540c585ab7af4bf40e129c0d92f7bd3d
SUNKARA NAVYA SRI SAI <navyasunkara049@gmail.com>
2025-09-20 15:09:47 +0530

    Time: 819 ms (11.1%), Space: 45.4 MB (21.61%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int cnt=0;
+       for(int i=0;i<s.length();i++)
+       {
+        for(int j=i+1;j<=s.length();j++)
+        {
+            if(ispalin(s.substring(i,j)))
+            {
+                cnt++;
+            }
+        }
+       }
+       return cnt;
+    }
+    public boolean ispalin(String s) {
+        
+        int i=0;
+        int j=s.length()-1;
+        while(i<j)
+        {
+            if(s.charAt(i)!=s.charAt(j))
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
